f0b655f9c72e2d11809b6c0c1aac16163e928911
ColumbusTech <ColumbusTech@yandex.ru>
2018-02-14 14:43:02 +0300

    Cubic meters

--- GUI.java
+++ GUI.java
@@ -4,9 +4,12 @@ import javax.swing.*;
 
 public class GUI extends JFrame
 {
+	private String[] modes = { "sm^3", "m^3" };
+
 	private JButton button;
-	private JLabel lx, ly, lz;
-	private JTextField x, y, z;
+	private JComboBox mode;
+	private JLabel lx, ly, lz, lpress, ldens;
+	private JTextField x, y, z, press, dens;
 	private JPanel panel;
 
 	public GUI(String title)
@@ -20,19 +23,26 @@ public class GUI extends JFrame
 		panel.setLayout(new GridLayout(0, 2));
 
 		button = new JButton("Calculate");
+		mode = new JComboBox(modes);
 
 		lx = new JLabel("Width (sm)");
 		ly = new JLabel("Height (sm)");
 		lz = new JLabel("Length (sm)");
+		lpress = new JLabel("Pressure (Pa)");
+		ldens = new JLabel("Density (g/sm^3)");
 
 		x = new JTextField("");
 		y = new JTextField("");
 		z = new JTextField("");
+		press = new JTextField("");
+		dens = new JTextField("");
 
 		panel.add(lx); panel.add(x);
 		panel.add(ly); panel.add(y);
 		panel.add(lz); panel.add(z);
-		panel.add(button);
+		panel.add(lpress); panel.add(press);
+		panel.add(ldens); panel.add(dens);
+		panel.add(button); panel.add(mode);
 		add(panel);
 		pack();
 
@@ -40,23 +50,49 @@ public class GUI extends JFrame
 		{
 			public void actionPerformed(ActionEvent e)
 			{
-				double dx, dy, dz;
-				double V;
+				double dx, dy, dz, dpress, ddens;
+				double V, Vs, Vc, m;
+				String format, result;
 
 				try
 				{
 					dx = Double.parseDouble(x.getText());
 					dy = Double.parseDouble(y.getText());
 					dz = Double.parseDouble(z.getText());
+					dpress = Double.parseDouble(press.getText());
+					ddens = Double.parseDouble(dens.getText());
+
+					if (dx <= 0.0 || dy <= 0.0 || dz <= 0.0 || dpress <= 0.0 || ddens <= 0.0)
+						throw new NumberFormatException();
 				} catch (NumberFormatException nfe)
 				{
-					JOptionPane.showMessageDialog(null, "Data is incorrect");
+					JOptionPane.showMessageDialog(null, "Data is incorrect", "Error", JOptionPane.ERROR_MESSAGE);
 					return;
 				}
 
 				V = dx * dy * dz;
+				m = dpress / (dx * dy) / 10;
+				Vs = m * ddens;
+				Vc = V - Vs;
 
-				JOptionPane.showMessageDialog(null, "Amount: " + V + " sm^3");
+				switch (mode.getSelectedIndex())
+				{
+				case 0: format = " sm^3"; break;
+				case 1:
+					format = " m^3";
+					V /= 1000000;
+					Vs /= 1000000;
+					Vc /= 1000000;
+					break;
+				default: format = " sm^3";  break;
+				}
+
+				result = "Volume: " + V + format + "\n";
+				result += "Substance volume: " + Vs + format + "\n";
+				result += "Cavity volume: " + Vc + format + "\n";
+				result += "Mass: " + m + " kg" + "\n";
+
+				JOptionPane.showMessageDialog(null, result, "Result", JOptionPane.PLAIN_MESSAGE);
 			}
 		});
 	}
